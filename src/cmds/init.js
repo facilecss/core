f41e73b219d5418775fde71e51ae336a8d67b3ef
lassev05 <vestergaardlasse2@gmail.com>
2022-07-04 10:40:01 +0200

    Init cmd is done!

--- src/cmds/init.js
+++ src/cmds/init.js
@@ -4,6 +4,51 @@
  *===========================================================================================*/
 
 const chalk = require("chalk");
-const config = require("../config");
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
 
-console.log("d");
+let questions = [
+    {
+        type: "input",
+        name: "projectName",
+        message: "What is the name of your project?",
+        default: "my-project",
+        validate: function (input) {
+            if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+            else
+                return "Project name may only include letters, numbers, underscores and hashes.";
+        },
+    },
+];
+
+inquirer.prompt(questions).then((answers) => {
+    const configCompiler = require("../config");
+
+    let configContent = `
+module.exports = {
+  projectName: "${answers.projectName}",
+  version: "${configCompiler.compiler.version}",
+
+  settings: {
+    files: ['.html'], // this is the type of files facile will compile.
+    outDir: "dist", // you can change to your custom choice, or leave it empty.
+    outFile: "facile.build.css", // the default output file name is facile.build.css
+
+     }
+  };
+    `;
+
+    let time = Date.now();
+
+    const configFile = path.join(process.cwd(), configCompiler.compiler.file);
+
+    fs.writeFileSync(configFile, configContent);
+
+    console.log(chalk.cyanBright("✅ Your config file has been created."));
+    console.log(
+        chalk.hex(configCompiler.colors.pink)(
+            `⏰ Done in ${chalk.gray(Date.now() - time + "ms!")}`
+        )
+    );
+});
---
+++ src/facile.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    projectName: "my-project",
+    version: "0.1.1-beta.0",
+
+    settings: {
+        files: [".html"], // this is the type of files facile will compile.
+        outDir: "dist", // you can change to your custom choice, or leave it empty.
+        outFile: "facile.build.css", // the default output file name is facile.build.css
+    },
+};
