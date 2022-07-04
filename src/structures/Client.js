d73d1666891165dcfd8cfc916c846b242f85c656
lassev05 <vestergaardlasse2@gmail.com>
2022-07-04 10:55:07 +0200

    Updated Client

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -1,27 +1,30 @@
 const chalk = require("chalk");
 
 class Client {
-  constructor(options) {
-    this.version = options.version;
-    this.name = options.name;
-    this.description = options.description;
-    this.links = options.links;
-  }
-
-  get(key) {
-    return this[key];
-  }
-
-  run() {
-    if (!this.version || !this.name || !this.description || !this.links) {
-      console.log(
-        chalk.red("❌ Client not initialized, please set all options.")
-      );
-      return;
-    } else {
-      console.log(chalk.green("✅ No errors detected."));
+    constructor(options) {
+        this.version = options.version;
+        this.name = options.name;
+        this.description = options.description;
+        this.links = options.links;
     }
-  }
+
+    get(key) {
+        return this[key];
+    }
+
+    run() {
+        if (!this.version || !this.name || !this.description || !this.links) {
+            console.log(
+                chalk.red(
+                    "❌ Detected missing required fields in your config file."
+                )
+            );
+
+            return;
+        } else {
+            console.log(chalk.green("✅ No errors detected."));
+        }
+    }
 }
 
 module.exports = Client;
