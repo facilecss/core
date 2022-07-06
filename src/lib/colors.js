bd66c5c025acc06e93010db7680343f6ad8564d3
lassev05 <vestergaardlasse2@gmail.com>
2022-07-06 11:37:23 +0200

    More classes builded and testst!

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -28,16 +28,74 @@ let colorsClasses = [
     },
 ];
 
-function genarateClasses() {
-    colorsClasses.forEach((colorClass) => {
-        Object.keys(colorClass.values).forEach((key) => {
-            colorClass.values[
-                key
-            ] = `.${colorClass.prefix}-${key} { color: ${colorClass.values[key]}; }`;
-        });
+function genarateShades() {
+    function shade(color, percent) {
+        let f = parseInt(color.slice(1), 16),
+            t = percent < 0 ? 0 : 255,
+            p = percent < 0 ? percent * -1 : percent,
+            R = f >> 16,
+            G = (f >> 8) & 0x00ff,
+            B = f & 0x0000ff;
+
+        return (
+            "#" +
+            (
+                0x1000000 +
+                (Math.round((t - R) * p) + R) * 0x10000 +
+                (Math.round((t - G) * p) + G) * 0x100 +
+                (Math.round((t - B) * p) + B)
+            )
+                .toString(16)
+                .slice(1)
+        );
+    }
+
+    function darken(color, percent) {
+        return shade(color, percent);
+    }
+
+    function lighten(color, percent) {
+        return shade(color, percent);
+    }
+
+    colorsClasses.push({
+        name: "color-darken",
+        prefix: "darken",
+        values: {
+            0: "0",
+            1: darken(colors.primary, 0.1),
+            2: darken(colors.primary, 0.2),
+            3: darken(colors.primary, 0.3),
+            4: darken(colors.primary, 0.4),
+            5: darken(colors.primary, 0.5),
+            6: darken(colors.primary, 0.6),
+            7: darken(colors.primary, 0.7),
+            8: darken(colors.primary, 0.8),
+            9: darken(colors.primary, 0.9),
+            10: darken(colors.primary, 1),
+        },
+
+        // color-lighten
+        name: "color-lighten",
+        prefix: "lighten",
+        values: {
+            0: "0",
+            1: lighten(colors.primary, 0.1),
+            2: lighten(colors.primary, 0.2),
+            3: lighten(colors.primary, 0.3),
+            4: lighten(colors.primary, 0.4),
+            5: lighten(colors.primary, 0.5),
+            6: lighten(colors.primary, 0.6),
+            7: lighten(colors.primary, 0.7),
+            8: lighten(colors.primary, 0.8),
+            9: lighten(colors.primary, 0.9),
+            10: lighten(colors.primary, 1),
+        },
     });
+
+    console.log(colorsClasses);
 }
 
-console.log(genarateClasses());
+console.log(genarateShades());
 
 module.exports = colors;
--- src/lib/utilityClasses.js
+++ src/lib/utilityClasses.js
@@ -17,9 +17,12 @@ const rootVar = {
     baseMargin: "0.75rem",
 
     // borders
+    baseBoxShadow: "1px 3px 5px rgba(0, 0, 0, 0.1);",
 };
 
 const classes = [
+    // fonts
+
     {
         name: "font-size",
         prefix: "font",
@@ -134,6 +137,184 @@ const classes = [
             10: rootVar.basePadding * 10 + "rem",
         },
     },
+
+    {
+        name: "margin",
+        prefix: "m",
+        values: {
+            0: "0",
+            1: rootVar.basePadding + "rem",
+            2: rootVar.basePadding * 2 + "rem",
+            3: rootVar.basePadding * 3 + "rem",
+            4: rootVar.basePadding * 4 + "rem",
+            5: rootVar.basePadding * 5 + "rem",
+            6: rootVar.basePadding * 6 + "rem",
+            7: rootVar.basePadding * 7 + "rem",
+            8: rootVar.basePadding * 8 + "rem",
+            9: rootVar.basePadding * 9 + "rem",
+            10: rootVar.basePadding * 10 + "rem",
+        },
+    },
+
+    {
+        name: "margin",
+        prefix: "m",
+        values: {
+            0: "0",
+            1: rootVar.basePadding + "rem",
+            2: rootVar.basePadding * 2 + "rem",
+            3: rootVar.basePadding * 3 + "rem",
+            4: rootVar.basePadding * 4 + "rem",
+            5: rootVar.basePadding * 5 + "rem",
+            6: rootVar.basePadding * 6 + "rem",
+            7: rootVar.basePadding * 7 + "rem",
+            8: rootVar.basePadding * 8 + "rem",
+            9: rootVar.basePadding * 9 + "rem",
+            10: rootVar.basePadding * 10 + "rem",
+        },
+    },
+
+    {
+        name: "margin-left",
+        prefix: "ml",
+        values: {
+            0: "0",
+            1: rootVar.basePadding + "rem",
+            2: rootVar.basePadding * 2 + "rem",
+            3: rootVar.basePadding * 3 + "rem",
+            4: rootVar.basePadding * 4 + "rem",
+            5: rootVar.basePadding * 5 + "rem",
+            6: rootVar.basePadding * 6 + "rem",
+            7: rootVar.basePadding * 7 + "rem",
+            8: rootVar.basePadding * 8 + "rem",
+            9: rootVar.basePadding * 9 + "rem",
+            10: rootVar.basePadding * 10 + "rem",
+        },
+    },
+
+    {
+        name: "margin-right",
+        prefix: "mr",
+        values: {
+            0: "0",
+            1: rootVar.basePadding + "rem",
+            2: rootVar.basePadding * 2 + "rem",
+            3: rootVar.basePadding * 3 + "rem",
+            4: rootVar.basePadding * 4 + "rem",
+            5: rootVar.basePadding * 5 + "rem",
+            6: rootVar.basePadding * 6 + "rem",
+            7: rootVar.basePadding * 7 + "rem",
+            8: rootVar.basePadding * 8 + "rem",
+            9: rootVar.basePadding * 9 + "rem",
+            10: rootVar.basePadding * 10 + "rem",
+        },
+    },
+
+    {
+        name: "margin-bottom",
+        prefix: "mb",
+        values: {
+            0: "0",
+            1: rootVar.basePadding + "rem",
+            2: rootVar.basePadding * 2 + "rem",
+            3: rootVar.basePadding * 3 + "rem",
+            4: rootVar.basePadding * 4 + "rem",
+            5: rootVar.basePadding * 5 + "rem",
+            6: rootVar.basePadding * 6 + "rem",
+            7: rootVar.basePadding * 7 + "rem",
+            8: rootVar.basePadding * 8 + "rem",
+            9: rootVar.basePadding * 9 + "rem",
+            10: rootVar.basePadding * 10 + "rem",
+        },
+    },
+
+    {
+        name: "margin-top",
+        prefix: "mt",
+        values: {
+            0: "0",
+            1: rootVar.basePadding + "rem",
+            2: rootVar.basePadding * 2 + "rem",
+            3: rootVar.basePadding * 3 + "rem",
+            4: rootVar.basePadding * 4 + "rem",
+            5: rootVar.basePadding * 5 + "rem",
+            6: rootVar.basePadding * 6 + "rem",
+            7: rootVar.basePadding * 7 + "rem",
+            8: rootVar.basePadding * 8 + "rem",
+            9: rootVar.basePadding * 9 + "rem",
+            10: rootVar.basePadding * 10 + "rem",
+        },
+    },
+
+    // Shadows
+    {
+        name: "box-shadow",
+        prefix: "bs",
+        values: {
+            none: "none",
+            xs: "0 0 0 0.125rem rgba(0,0,0,0.25)",
+            sm: "0 0 0 0.25rem rgba(0,0,0,0.25)",
+            md: "0 0 0 0.375rem rgba(0,0,0,0.25)",
+            lg: "0 0 0 0.5rem rgba(0,0,0,0.25)",
+            xl: "0 0 0 0.75rem rgba(0,0,0,0.25)",
+            xxl: "0 0 0 1rem rgba(0,0,0,0.25)",
+        },
+    },
+
+    {
+        name: "text-shadow",
+        prefix: "ts",
+        values: {
+            none: "none",
+            xs: "0 0 0.125rem rgba(0,0,0,0.25)",
+            sm: "0 0 0.25rem rgba(0,0,0,0.25)",
+            md: "0 0 0.375rem rgba(0,0,0,0.25)",
+            lg: "0 0 0.5rem rgba(0,0,0,0.25)",
+            xl: "0 0 0.75rem rgba(0,0,0,0.25)",
+            xxl: "0 0 1rem rgba(0,0,0,0.25)",
+        },
+    },
+
+    // Cursors
+    {
+        name: "cursor",
+        prefix: "c",
+        values: {
+            default: "default",
+            pointer: "pointer",
+            text: "text",
+            wait: "wait",
+            move: "move",
+            "not-allowed": "not-allowed",
+            help: "help",
+            auto: "auto",
+            crosshair: "crosshair",
+            "no-drop": "no-drop",
+            none: "none",
+            "context-menu": "context-menu",
+            progress: "progress",
+            cell: "cell",
+            grab: "grab",
+            grabbing: "grabbing",
+            alias: "alias",
+            "all-scroll": "all-scroll",
+            "zoom-in": "zoom-in",
+            "zoom-out": "zoom-out",
+        },
+    },
+
+    // Height
+    (function () {
+        var height = {};
+        for (var i = 0; i <= 35; i++) {
+            height[i] = i + "rem";
+        }
+        return {
+            name: "height",
+            prefix: "h",
+            values: height,
+        };
+    })(),
 ];
 
 function genarateClasses() {
