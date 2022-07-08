37469d5f5e2e31fa3df4567da15e2def9953206a
lassev05 <vestergaardlasse2@gmail.com>
2022-07-08 15:12:13 +0200

    Update color shades

--- src/colors.js
+++ src/colors.js
@@ -17,42 +17,121 @@ const colors = [
 
     {
         name: 'secondary',
-        hexCode: '#f5f5f5',
+        shades: {
+            default: '#f5f5f5',
+            1: '#f2f2f2',
+            2: '#d9d9d9',
+            3: '#bfbfbf',
+            4: '#a6a6a6',
+            5: '#8c8c8c',
+            6: '#737373',
+            7: '#595959',
+            8: '#404040',
+            9: '#262626',
+        },
     },
 
     {
         name: 'error',
-        hexCode: '#d32752',
+
+        shades: {
+            default: '#d32752',
+            1: '#fbe9ee',
+            2: '#f3becc',
+            3: '#eb93a9',
+            4: '#e36887',
+            5: '#db3d65',
+            6: '#db3d65',
+            7: '#c2244b',
+            8: '#971c3b',
+            9: '#6c142a',
+        },
     },
 
     {
         name: 'info',
-        hexCode: '#00bcd4',
+        shade: {
+            default: '#00bcd4',
+            1: '#e5fcff',
+            2: '#b3f6ff',
+            3: '#80f1ff',
+            4: '#4debff',
+            5: '#1ae5ff',
+            6: '#00cce6',
+            7: '#009eb3',
+            8: '#007180',
+            9: '#00444d',
+        },
     },
 
     {
         name: 'teal',
-        hexCode: '#009688',
+        shades: {
+            default: '#009688',
+            1: '#e5fffd',
+            2: '#b3fff8',
+            3: '#80fff3',
+            4: '#1affea',
+            5: '#00e6d0',
+            6: '#00b3a2',
+            7: '#008074',
+            8: '#004d45',
+            9: '#00463f',
+        },
     },
 
     {
         name: 'blue',
-        hexCode: '#1919e6',
+        shades: {
+            default: '#1919e6',
+            1: '#e8e8fc',
+            2: '#babaf7',
+            3: '#8c8cf2',
+            4: '#5e5eed',
+            5: '#3030e8',
+            6: '#1717cf',
+            8: '#1212a1',
+            9: '#0d0d73',
+        },
     },
 
     {
         name: 'red',
-        hexCode: '#e61919',
+        shades: {
+            default: '#e61919',
+            1: '#fce8e8',
+            2: '#f7baba',
+            3: '#f28c8c',
+            4: '#ed5e5e',
+            5: '#e83030',
+            6: '#cf1717',
+            7: '#a11212',
+            8: '#730d0d',
+            9: '#450808',
+        },
     },
 
     {
         name: 'yellow',
-        hexCode: '#e6e619',
+        shades: {
+            default: '#e6e619',
+            1: '#fcfce8',
+            2: '#f7f7ba',
+            3: '#f2f28c',
+            4: '#eded5e',
+            5: '#e8e830',
+            6: '#cfcf17',
+            7: '#a1a112',
+            8: '#73730d',
+            9: '#454508',
+        },
     },
 
     {
         name: 'green',
-        hexCode: '#19e635',
+        shades: {
+            default: '#19e635',
+        },
     },
 
     {
