49d19390ec3b6fe277fb3ff022070cd3a650cdb9
lassev05 <vestergaardlasse2@gmail.com>
2022-07-08 14:49:53 +0200

    Create first part of color pattlete

---
+++ src/colors.js
@@ -0,0 +1,84 @@
+const colors = [
+    {
+        name: 'primary',
+        shades: {
+            default: '#ea4442',
+            1: '#fce6e5',
+            2: '#f6b3b2',
+            3: '#f1817f',
+            4: '#eb4e4d',
+            5: '#e51c1a',
+            6: '#b21614',
+            7: '#7f100e',
+            8: '#4c0909',
+            9: '#3f0807',
+        },
+    },
+
+    {
+        name: 'secondary',
+        hexCode: '#f5f5f5',
+    },
+
+    {
+        name: 'error',
+        hexCode: '#d32752',
+    },
+
+    {
+        name: 'info',
+        hexCode: '#00bcd4',
+    },
+
+    {
+        name: 'teal',
+        hexCode: '#009688',
+    },
+
+    {
+        name: 'blue',
+        hexCode: '#1919e6',
+    },
+
+    {
+        name: 'red',
+        hexCode: '#e61919',
+    },
+
+    {
+        name: 'yellow',
+        hexCode: '#e6e619',
+    },
+
+    {
+        name: 'green',
+        hexCode: '#19e635',
+    },
+
+    {
+        name: 'orange',
+        hexCode: '#ffa600',
+    },
+
+    {
+        name: 'purple',
+        hexCode: '#9900ff',
+    },
+
+    {
+        name: 'gray',
+        hexCode: '#808080',
+    },
+
+    {
+        name: 'black',
+        hexCode: 'black',
+    },
+
+    {
+        name: 'white',
+        hexCode: 'white',
+    },
+]
+
+module.exports = { colors }
