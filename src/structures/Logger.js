933c584103fbf82acd03463dd20e31cdd5286858
lassev05 <vestergaardlasse2@gmail.com>
2022-07-19 01:14:32 +0200

    Added copyrigt comment in Logger

--- src/structures/Logger.js
+++ src/structures/Logger.js
@@ -1,3 +1,8 @@
+/*===========================================================================================
+ *  Copyright (c) Facil CSS authors. All rights reserved.
+ *  Licensed under the MIT License. Read the LICENSE file, for more information.
+ *===========================================================================================*/
+
 const chalk = require('chalk')
 const moment = require('moment')
 
