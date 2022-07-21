27c09be1692cc3c9a81263d6c162494900c3db19
lassev05 <vestergaardlasse2@gmail.com>
2022-07-21 03:53:10 +0200

    Build all files together.

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -10,16 +10,33 @@ const path = require('path')
 const child_process = require('child_process')
 const chalk = require('chalk')
 const Logger = require('./classes/Logger')
+const Spinner = require('cli-spinner').Spinner
 
 function getCSSFiles(dir) {
     return fs.readdirSync(dir).filter((file) => file.endsWith('.css'))
 }
 
-getCSSFiles(path.join(__dirname, './components')).forEach((file) => {
+getCSSFiles(path.join(__dirname, './css')).forEach((file) => {
     const fileContents = fs.readFileSync(
-        path.join(__dirname, './components', file),
+        path.join(__dirname, './css', file),
         'utf8'
     )
 
     new Logger('info').log(`Processing ${file}`)
+
+    fs.writeFileSync(
+        path.join(__dirname, './dist', 'bundle.css'),
+        fileContents,
+        {
+            flag: 'a',
+        }
+    )
+
+    const spinner = new Spinner('Running tailwindcss... %s')
+    spinner.setSpinnerString('|/-\\')
+    spinner.start()
+
+    child_process.execSync(`pnpm run dev:tailwindcss`, {
+        stdio: 'inherit',
+    })
 })
