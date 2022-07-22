83c48892dec79e2944e284b52ff366a5e0eff6fd
lassev05 <vestergaardlasse2@gmail.com>
2022-07-22 23:52:57 +0200

    Fixed Badges (border-10 build issue)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 'latest',
     },
-    "rules": {
-    }
+    rules: {},
 }
